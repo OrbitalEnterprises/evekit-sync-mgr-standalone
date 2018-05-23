482313618ec73b19802bfac2238a153245874418
deadlybulb <deadlybulb@orbital.enterprises>
2018-05-23 07:34:08 -0400

    release sync threads if we can't obtain locks in a reasonable amount of time

--- src/main/java/enterprises/orbital/evekit/sync/account/ESIStandardAccountSyncEvent.java
+++ src/main/java/enterprises/orbital/evekit/sync/account/ESIStandardAccountSyncEvent.java
@@ -1,15 +1,15 @@
 package enterprises.orbital.evekit.sync.account;
 
-import enterprises.orbital.base.OrbitalProperties;
 import enterprises.orbital.evekit.account.SynchronizedEveAccount;
 import enterprises.orbital.evekit.model.ESIAccountSynchronizationHandler;
 import enterprises.orbital.evekit.model.ESISyncEndpoint;
 import enterprises.orbital.evekit.sync.ControllerEvent;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ESIStandardAccountSyncEvent extends ControllerEvent implements Runnable {
@@ -56,16 +56,27 @@ public class ESIStandardAccountSyncEvent extends ControllerEvent implements Runn
   public void run() {
     log.fine("Starting execution: " + toString());
     super.run();
-    // For fairness, and to prevent certain data races, serialize sync activities on a
-    // per-sync account basis.  This doesn't really guarantee fairness if a single account
-    // has many active threads.  Fortunately, this still works pretty well since sync
-    // times for endpoints are reasonably diverse.
+    // Sync activities on a per-account basis to prevent certain data races.
+    // Note that we bound the time we wait for the lock to avoid starving
+    // other tasks queued in the thread pool.
     ReentrantLock lck = SynchronizedEveAccount.getSyncAccountLock(handler.account());
-    lck.lock();
+    int maxDelay = ThreadLocalRandom.current()
+                                    .nextInt(30);
     try {
+      // Wait no less than 30 and no more than 60 sexonds for the lock.  If we fail
+      // to obtain the lock, then yield this thread and wait for account checker to
+      // reschedule us.
+      if (!lck.tryLock(30 + maxDelay, TimeUnit.SECONDS)) {
+        log.fine("Failed to obtain lock, yielding thread: " + toString());
+        return;
+      }
       handler.synch(new AccountSyncClientProvider(scheduler));
+    } catch (InterruptedException e) {
+      log.log(Level.WARNING, "Interrupted while waiting for lock", e);
     } finally {
-      lck.unlock();
+      // Must verify we hold the lock first since the case where we fail to
+      // obtain the lock is included in the same try block.
+      if (lck.isHeldByCurrentThread()) lck.unlock();
     }
     log.fine("Execution complete: " + toString());
   }
