67b4383996be5fa38a73311c66ef097313b90b4a
deadlybulb <deadlybulb@orbital.enterprises>
2018-11-21 09:37:50 -0500

    make shard bucket count configurable

--- src/main/java/enterprises/orbital/evekit/sync/account/ShardManager.java
+++ src/main/java/enterprises/orbital/evekit/sync/account/ShardManager.java
@@ -1,6 +1,10 @@
 package enterprises.orbital.evekit.sync.account;
 
+import java.util.logging.Logger;
+
 public class ShardManager {
+  public static final Logger log = Logger.getLogger(ShardManager.class.getName());
+
   // Allowed sharding strategies
   enum Strategies {
     SINGLE_HASH
@@ -12,13 +16,26 @@ public class ShardManager {
   public static ShardFilter createShardFilter(Strategies strat, String init) {
     switch (strat) {
       case SINGLE_HASH:
-        final boolean[] filter = new boolean[10];
+        int shardBuckets = 10;
+        if (init.indexOf(':') != -1) {
+          String[] initSplit = init.split(":");
+          shardBuckets = Integer.valueOf(initSplit[0]);
+          init = initSplit[1];
+        }
+        final int bucketCount = shardBuckets;
+        final boolean[] filter = new boolean[bucketCount];
+        StringBuilder processList = new StringBuilder();
+        processList.append("[ ");
         for (String i : init.split(",")) {
           int offset = Integer.valueOf(i);
           filter[offset] = true;
+          processList.append(offset).append(' ');
         }
+        processList.append(']');
+        log.info("Instantiating SINGLE_HASH shard strategy with " + String.valueOf(bucketCount) + " buckets");
+        log.info("This instance will process buckets: " + processList.toString());
         return acct -> {
-          int index = (int) (acct.getAid() % 10);
+          int index = (int) (acct.getAid() % bucketCount);
           return filter[index];
         };
 
